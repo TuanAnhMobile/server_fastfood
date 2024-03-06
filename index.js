c7c8d550a6e3116cb7342cd267b0230b0bc5c2e0
quoccon <quocnvph23753@fpt.edu.vn>
2024-03-06 13:30:00 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT,() => {
+    console.log("Server listening on port : " + PORT);
+});
---
+++ model/user.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose');
+const db = require('./db');
+
+const userChema = new db.mongoose.Schema(
+    {
+        username: { type: String },
+        email: { type: String },
+        phone: { type: Number },
+        password: { type: String },
+        cart: [{ type: mongoose.Schema.Types.ObjectId, ref: 'userModel' }],
+    }, {
+    collection: 'users'
+}
+)
+
+let userModel = db.mongoose.model('userModel', userChema);
+
+module.exports = { userModel }
